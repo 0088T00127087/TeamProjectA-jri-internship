933bc9a5a9579a1eb2a9e9b430f5df907a2b9498
ian20_000 <ian20_000@Tom>
2018-06-06 11:48:47 +0100

    Test

--- ProjectA-Backend/src/main/java/com/systems/ProjectABackendApplication.java
+++ ProjectA-Backend/src/main/java/com/systems/ProjectABackendApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectABackendApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectABackendApplication.class, args);
 	}
