412af5bca2adbdcee9da2a2476949b1aa4162a34
ian20_000 <ian20_000@Tom>
2018-06-06 11:46:52 +0100

    Initial Spring Setup - Not Functional

---
+++ ProjectA-Backend/src/main/java/com/systems/ProjectABackendApplication.java
@@ -0,0 +1,12 @@
+package com.systems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectABackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectABackendApplication.class, args);
+	}
+}
