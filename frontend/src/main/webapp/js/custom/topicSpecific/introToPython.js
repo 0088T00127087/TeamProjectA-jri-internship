497d69e9d98a6fd9604e6a56f9d1aad85fea045a
ian20_000 <ian20_000@Tom>
2018-07-29 15:53:29 +0100

    Minor UI Video Adjustments & Question Modal Initial Load

--- frontend/src/main/webapp/js/custom/topicSpecific/introToPython.js
+++ frontend/src/main/webapp/js/custom/topicSpecific/introToPython.js
@@ -8,6 +8,15 @@ $(document).ready(function(){
 	window.history.replaceState({}, document.title, "/frontend/pages/introductionToPython.html");
 	customisation();
 	retrieveQuestions();
+	videoValidity();
+	$("#homepageVideo").get(0).prop("volume", this.value);
+	$('#volume').on('change', function() {
+		$("#homepageVideo").get(0).prop("volume", this.value);
+	});
+});
+
+$(function() {
+    $(document).scrollTop( $("#homepageVideo").offset().top );  
 });
 
 function retrieveQuestions(){
@@ -30,4 +39,48 @@ function customisation(){
 	}, function(response){
 		$("#name").text(response);
 	});	
+}
+
+function videoValidity(){
+//	$.get({
+//		url: "/frontend/VideoPermissionsWebServlet",
+//		cache: false,		
+//		type : "GET",
+//		data : {videoNumber: "1"}
+//	}, function(validView){
+	//	if (validView === "valid"){
+			$("#homepageVideo").get(0).play();
+//			$("#homepageVideo").on("timeupdate", 
+//					function(event){
+//				onTrackedVideoFrame(this.currentTime, this.duration);
+//			});
+			$('#homepageVideo').on('ended',function(){
+				questionModalLoader();
+		//		adjustVideoPermissions();
+			});	
+		//} 
+//		else {
+//			console.log(validView);
+//		}
+//	});
+}
+
+function questionModalLoader(){
+	$('#questionModal').modal({backdrop: 'static', keyboard: false})
+}
+
+
+function fullscreenVideo(){
+    if($("#homepageVideo").get(0).requestFullscreen){
+    	$("#homepageVideo").get(0).requestFullscreen();
+    } 
+    else if ($("#homepageVideo").get(0).webkitRequestFullscreen){
+    	$("#homepageVideo").get(0).webkitRequestFullscreen();
+    }
+    else if ($("#homepageVideo").get(0).mozRequestFullScreen){
+    	$("#homepageVideo").get(0).mozRequestFullScreen();
+    }
+    else if ($("#homepageVideo").get(0).msRequestFullscreen){
+    	$("#homepageVideo").get(0).msRequestFullscreen();
+    } 
 }
